d5edae03eb0766182bafdd2a29686fbb6c802b2b
dudaodong <dudaodong@kingsoft.com>
2019-06-06 18:01:31 +0800

    update file

---
+++ src/components/testcomponent/TestComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello world</div>
+</template>
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="stylus"></style>
---
+++ src/views/layout/Loyout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">hello world</div>
+</template>
+<script>
+export default {
+  name: "Layout",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="stylus">
+.layout {
+}
+</style>
